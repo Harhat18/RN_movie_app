6bb315d6a438d3651182444795ceddab0b0a20f0
Harhat18 <harunhatib18@gmail.com>
2023-10-17 15:00:03 +0300

    Add first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View class>
+        <SafeAreaView>
+          <Text>aa</Text>
+        </SafeAreaView>
+      </View>
+    );
+  }
+}
