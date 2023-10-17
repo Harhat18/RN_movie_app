62255075529e9093100fbb91471e5c4070d15918
Harhat18 <harunhatib18@gmail.com>
2023-10-17 22:52:14 +0300

    created movilist and home screen trendig movies

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,52 @@
-import { Text, View } from "react-native";
-import React, { Component } from "react";
+import {
+  Platform,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { Component, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
 
-export default class HomeScreen extends Component {
-  render() {
-    return (
-      <View class>
-        <SafeAreaView>
-          <Text>aa</Text>
-        </SafeAreaView>
-      </View>
-    );
-  }
+import TrendingMovies from "../components/trendingMovies";
+import MovieList from "../components/movieList";
+
+const ios = Platform.OS === "ios";
+export default function HomeScreen() {
+  const [trending, setTrending] = useState([1, 2, 3]);
+  const [upcoming, setUpcoming] = useState([1, 2, 3]);
+  const [topRated, setTopRated] = useState([1, 2, 3]);
+  return (
+    <View className="flex-1 bg-neutral-800 ">
+      <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text className="text-white font-bold text-3xl">
+            <Text style={styles.text}>M</Text>ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
+      >
+        {/* Tending movies carusel*/}
+        <TrendingMovies data={trending} />
+        {/* upcoming movies row*/}
+        <MovieList title="Upcoming" data={upcoming} />
+        {/* top rated movies row*/}
+        <MovieList title="Top Rated" data={topRated} />
+      </ScrollView>
+    </View>
+  );
 }
