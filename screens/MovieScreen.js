f89ab6014fc5263f93e9230b5c037bbe49d044c9
Harhat18 <harunhatib18@gmail.com>
2023-10-18 11:13:01 +0300

    created Moviescreen

---
+++ screens/MovieScreen.js
@@ -0,0 +1,117 @@
+import {
+  View,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+  Text,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { styles, theme } from "../theme";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { LinearGradient } from "expo-linear-gradient";
+import Cast from "../components/cast";
+
+const ios = Platform.OS == "ios";
+const topMargin = ios ? "" : " mt-3";
+var { width, height } = Dimensions.get("window");
+
+export default function MovieScreen() {
+  const { params: item } = useRoute();
+  const navigation = useNavigation();
+
+  const [movie, setMovie] = useState({});
+  const [cast, setCast] = useState([1, 2, 3, 4, 5]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [isFavourite, toggleFavourite] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  let movieName = " Ant-man and the wasp";
+
+  useEffect(() => {
+    return () => {
+      // call details api
+    };
+  }, [item]);
+  return (
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 50 }}
+      className="flex-1 bg-neutral-900"
+    >
+      <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+      {/* back button and movie poster*/}
+      <View>
+        <SafeAreaView
+          className={
+            "absolute z-20 w-full flex-row justify-between items-center px-4 " +
+            topMargin
+          }
+        >
+          <TouchableOpacity
+            style={styles.background}
+            className="rounded-xl p-1"
+            onPress={() => navigation.goBack()}
+          >
+            <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+            <HeartIcon
+              size="35"
+              color={isFavourite ? theme.background : "white"}
+            />
+          </TouchableOpacity>
+        </SafeAreaView>
+        <View>
+          <Image
+            source={require("../assets/images/moviePoster2.png")}
+            style={{ width, height: height * 0.55 }}
+          />
+          <LinearGradient
+            colors={[
+              "transparent",
+              "rgba(23, 23, 23, 0.8)",
+              "rgba(23, 23, 23, 1)",
+            ]}
+            style={{ width, height: height * 0.4 }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+            className="absolute bottom-0"
+          />
+        </View>
+      </View>
+      <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+        {/* title */}
+        <Text className="text-white text-center text-3xl font-bold tracking-widest">
+          {movieName}
+        </Text>
+        <Text className="text-white text-center text-3xl font-bold tracking-widest">
+          2020
+        </Text>
+        {/* genres  */}
+        <View className="flex-row justify-center mx-4 space-x-2">
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Action •
+          </Text>
+        </View>
+        {/* description */}
+        <Text className="text-neutral-400 mx-4 tracking-wide">Super_hero</Text>
+      </View>
+
+      {/* cast */}
+      <Cast cast={cast} />
+
+      {/* similar movies section */}
+      {movie?.id && similarMovies.length > 0 && (
+        <MovieList
+          title={"Similar Movies"}
+          hideSeeAll={true}
+          data={similarMovies}
+        />
+      )}
+    </ScrollView>
+  );
+}
